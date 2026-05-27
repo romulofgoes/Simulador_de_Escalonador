60ccf71757ef301d424ead38915816cb80313262
romulofgoes <romulofgoes@proton.me>
2026-05-26 22:13:41 -0300

    fix: cpu nome PascalCase e sem time.sleep()

--- cpu.py
+++ cpu.py
@@ -1,16 +1,15 @@
 from processo import Processo
 import time
 
-class cpu: #processador - serão 4 instâncias (?)
+class Cpu: #processador - serão 4 instâncias (?)
     def __init(self):
         self.sinalInterrupcao=False # sinal de interrupção - para política de feedback que para o processo para ver o sinal de interrupção
     
     def interrupcao(self): # recebeu sinal de interrupção
         self.sinalInterrupcao = True
 
-    def executa(processo): # essa função executa - decrementa fase_1 ou fase_2 do processo
-        if(not processo.foiFinalizado() and not processo.foiInterrompido): # para quando processo é interrompido ou finalizado
-            time.sleep(1)
+    def executa(self, processo): # essa função executa - decrementa fase_1 ou fase_2 do processo
+        if(not processo.foiFinalizado() and not processo.foiInterrompido()): # para quando processo é interrompido ou finalizado
             processo.executaCiclo()
         
         else:
